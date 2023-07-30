7abb60ae6c841a9f9b1299fd0bdf84b1a8d26214
Konrad Pagacz <konrad.pagacz@gmail.com>
2023-07-30 18:57:19 +0200

    docs: added more information about the token to the help (#48)
    * Added more information and some cross-referencing to the help
      sections of the commands.
    
    Closes #43

--- src/application/cli/cli_config_subcommand.rs
+++ src/application/cli/cli_config_subcommand.rs
@@ -1,14 +1,16 @@
 #[derive(clap::Parser, Debug)]
 pub enum ConfigSubcommand {
-    /// Lists all the configuration keys and their respective values
+    /// List all the configuration keys and their respective values
     #[command(visible_aliases = ["l"])]
     List {},
 
-    /// Updates the value of the specified configuration key
+    /// Update the value of the specified configuration key
     ///
     /// Examples:
     /// elv config set aoc.token abscdft123145
     /// elv config set cli.output_width 150
+    ///
+    /// See `elv config list` for all available configuration keys.
     #[command(verbatim_doc_comment)]
     Set {
         /// The updated configuration key
--- src/application/cli/cli_interface.rs
+++ src/application/cli/cli_interface.rs
@@ -3,8 +3,16 @@ use super::cli_command::CliCommand;
 /// 🎄 Your Advent of Code CLI 🎄
 ///
 /// This CLI is a tool to help you with your Advent of Code challenges.
+///
+/// Token management
+/// You need an Advent of Code session token to interact with its API. `elv`
+/// does not support authentication to the API on its own, so you need to
+/// get your token beforehand, and pass it to `elv`. There are a number of ways
+/// of setting the token. See `elv config set --help` if you want to set it
+/// once and not be bothered by passing it to the `--token` parameter every
+/// time you use the CLI.
 #[derive(Debug, clap::Parser)]
-#[command(version)]
+#[command(version, verbatim_doc_comment)]
 pub struct CliInterface {
     #[command(subcommand)]
     pub command: CliCommand,
