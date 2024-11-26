2f36df14b2a26182391521aeff1e1820574950fc
Adam Sasine <adam.sasine@gmail.com>
2024-11-26 12:33:05 -0500

    Added sanity check to test

--- src/infrastructure/configuration.rs
+++ src/infrastructure/configuration.rs
@@ -142,7 +142,9 @@ mod tests {
         let project_dirs = Configuration::get_project_directories();
         let config_dir = project_dirs.config_dir();
         std::fs::remove_dir_all(config_dir).ok();
-        let result =  Configuration::default().write_to_file();
+        assert!(!config_dir.exists()); // sanity check
+
+        let result = Configuration::default().write_to_file();
         assert!(result.is_ok());
         assert!(config_dir.exists());
     }
